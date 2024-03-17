d00c171be476e28a2e5e103afde578b5d1403598
Samet Candan <c._samet@hotmail.com>
2024-03-17 14:36:52 +0300

    yup errors and handleblur

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -29,56 +29,73 @@ const LoginForm = () => {
           <div>
             <label>Email</label>
             <input
+              onBlur={formik.handleBlur}
               name="email"
               onChange={formik.handleChange}
               type="text"
               placeholder="ör: mail@mail.com"
             />
 
-            {formik.errors.email && <p>{formik.errors.email} </p>}
+            {formik.errors.email && formik.touched.email && (
+              <p>{formik.errors.email} </p>
+            )}
           </div>
           <div>
             <label>Yaş</label>
             <input
+              onBlur={formik.handleBlur}
               name="age"
               onChange={formik.handleChange}
               type="number"
               placeholder="ör: 35"
             />
 
-            {formik.errors.age && <p>{formik.errors.age} </p>}
+            {formik.errors.age && formik.touched.age && (
+              <p>{formik.errors.age} </p>
+            )}
           </div>
           <div>
             <label>Şifre</label>
             <input
+              onBlur={formik.handleBlur}
               name="password"
               onChange={formik.handleChange}
               type="password"
               placeholder="••••••"
             />
-            {formik.errors.password && <p>{formik.errors.password} </p>}
+            {formik.errors.password && formik.touched.password && (
+              <p>{formik.errors.password} </p>
+            )}
           </div>
           <div>
             <label>Şifre Onay</label>
             <input
+              onBlur={formik.handleBlur}
               name="confirmPassword"
               onChange={formik.handleChange}
               type="password"
               placeholder="••••••"
             />
 
-            {formik.errors.confirmPassword && (
-              <p>{formik.errors.confirmPassword} </p>
-            )}
+            {formik.errors.confirmPassword &&
+              formik.touched.confirmPassword && (
+                <p>{formik.errors.confirmPassword} </p>
+              )}
           </div>
           <div className="check">
             <input
+              onBlur={formik.handleBlur}
               name="terms"
               onChange={formik.handleChange}
               type="checkbox"
               id="terms"
             />
-            <label htmlFor="terms">Koşulları Okudum ve Onaylıyorum.</label>
+            <label onBlur={formik.handleBlur} htmlFor="terms">
+              Koşulları Okudum ve Onaylıyorum.
+            </label>
+            {formik.errors.terms && formik.touched.terms && (
+              <p>{formik.errors.terms} </p>
+            )}
           </div>
           <button>Kaydol</button>
         </form>
--- src/pages/schema.js
+++ src/pages/schema.js
@@ -1,5 +1,8 @@
 import * as yup from "yup";
 
+const passwordRules =
+  "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{5,}$";
+
 export const schema = yup.object().shape({
   email: yup
     .string()
@@ -7,12 +10,19 @@ export const schema = yup.object().shape({
     .required("Zorunlu Alan"),
   age: yup
     .number()
-    .required()
+    .required("Zorunlu Alan")
     .positive()
     .integer()
     .min(18, "18  Yaşından küçükler Giremez")
     .max(100, "Yaşınız 100 den büyük olamaz"),
-  //   password: "",
-  //   confirmPassword: "",
-  //   terms: false,
+  password: yup
+    .string()
+    .min(5, "Şifre en az 5 karakter olmalı.")
+    .matches(passwordRules, "Lütfen daha güçlü bir şifre giriniz.")
+    .required("Zorunlu alan"),
+  confirmPassword: yup
+    .string()
+    .required("Zorunlu Alan")
+    .oneOf([yup.ref("password")], "Şifre uyuşmuyor"),
+  terms: yup.boolean().isTrue("Koşulları kabul etmeden devam edemezsiniz."),
 });
